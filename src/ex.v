931ba12b8f628f9a154aebb6bf7814d2f44a65b9
tarik-ibrahimovic <tarik.ibrahimovic@chili-chips.xyz>
2025-11-08 16:56:23 +0100

    fix syntax

--- src/ex.v
+++ src/ex.v
@@ -2,5 +2,5 @@ module ex (
     input wire [16:0] mac_result, // Q3.13
     output wire [7:0] ex_result   // Q1.6 
 );
-    ex_result = mac_result[7:0];
+    assign ex_result = mac_result[7:0];
 endmodule
