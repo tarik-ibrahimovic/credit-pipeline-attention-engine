b527c3338f779f0651572cf7fd61f611970555f9
tarik-ibrahimovic <tarik.ibrahimovic@chili-chips.xyz>
2025-11-09 10:42:09 +0100

    fix syntax error

--- src/ex.v
+++ src/ex.v
@@ -8,5 +8,5 @@ module ex (
     wire signed [8:0] nln2 = $signed((n_round << 5) + (n_round << 3) + (n_round << 2)) >>> 6; // n(2)*n = 3*ln(2) = 2.something, Q2.6
     wire signed [8:0] r = x - nln2; // Q0.7 =, should be between -0.35 and 0.35
 
-    assign ex_result = 1<<n * (1+r+r*r/2);
+    assign ex_result = 1<<n_round * (1+r+r*r/2);
 endmodule
